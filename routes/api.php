bee84e5a371d7c90f83d68b6984f788f1a1db998
Taras Kulyk <taraskrivoagency@gmail.com>
2020-07-08 14:06:46 +0300

    Start add vue

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('login', 'TestController@login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('login', 'TestController@login');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
